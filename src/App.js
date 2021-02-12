0dcd476a57532367de0a5031a3397afdaec7dc57
JJ <jh_86@hotmail.co.uk>
2021-02-12 09:50:49 +0000

    square button component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,87 @@
-import logo from "./logo.svg";
+import React from 'react';
 import "./App.css";
+import SqaureButton from "./buttonSquare/buttonSquare";
 
 function App() {
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Happy Hacking Bootcampers!</h1>
-      </header>
+        <h1>Tic-tac-toe</h1>
+        <SqaureButton />
+
+
     </div>
   );
 }
 
 export default App;
+
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button className="square">
+//         {/* TODO */}
+//       </button>
+//     );
+//   }
+// }
+
+// class Board extends React.Component {
+//   renderSquare(i) {
+//     return <Square />;
+//   }
+
+//   render() {
+//     const status = 'Next player: X';
+
+//     return (
+//       <div>
+//         <div className="status">{status}</div>
+//         <div className="board-row">
+//           {this.renderSquare(0)}
+//           {this.renderSquare(1)}
+//           {this.renderSquare(2)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(3)}
+//           {this.renderSquare(4)}
+//           {this.renderSquare(5)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(6)}
+//           {this.renderSquare(7)}
+//           {this.renderSquare(8)}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// class Game extends React.Component {
+//   render() {
+//     return (
+//       <div className="game">
+//         <div className="game-board">
+//           <Board />
+//         </div>
+//         <div className="game-info">
+//           <div>{/* status */}</div>
+//           <ol>{/* TODO */}</ol>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// // ========================================
+
+// ReactDOM.render(
+//   <Game />,
+//   document.getElementById('root')
+// );
---
+++ src/buttonSquare/buttonSquare.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function SqaureButton(){
+   
+          return (
+            <button className="square">
+            {/* TODO */}
+            </button>
+          );
+};
+
+export default SqaureButton;
+      
